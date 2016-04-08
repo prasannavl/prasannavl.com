5f5600caddbb2910a0922ff01d297ae1e34ece49
Prasanna V. Loganathar <pvl@prasannavl.com>
2016-04-09 03:38:23 +0530

    guarded handling of externals

--- src/modules/ext/googleAdSense.js
+++ src/modules/ext/googleAdSense.js
@@ -1,14 +1,18 @@
 export function show() {
-    let head = document.head;
-    let tags = Array.from(head.getElementsByClassName("g-ads"));
-    tags.forEach(x => x.remove());
-    let tag = document.createElement("script");
-    tag.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
-    tag.className = "g-ads";
-    head.appendChild(tag);
-    let data = window.adsbygoogle;
-    if (!data) {
-        window.adsbygoogle = data = [];
+    if (__DOM__) {
+        try {
+            let head = document.head;
+            let tags = Array.from(head.getElementsByClassName("g-ads"));
+            tags.forEach(x => x.remove());
+            let tag = document.createElement("script");
+            tag.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+            tag.className = "g-ads";
+            head.appendChild(tag);
+            let data = window.adsbygoogle;
+            if (!data) {
+                window.adsbygoogle = data = [];
+            }
+            data.push({});
+        } catch (err) { } //eslint-disable-line no-empty
     }
-    data.push({});
 }
