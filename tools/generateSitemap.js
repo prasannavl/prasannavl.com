07505cf4266c900238930772304996ce0d335ed9
Prasanna V. Loganathar <pvl@prasannavl.com>
2016-06-17 19:42:11 +0530

    refine sitemap generation

--- tools/generateSitemap.js
+++ tools/generateSitemap.js
@@ -1,9 +1,11 @@
 import sitemap from "sitemap";
 import utils from "./utils";
 import configConstantsFactory from "../configConstants";
+import path from "path";
+import fs from "fs-extra-promise";
+import chalk from "chalk";
 
-
-function run() {
+function runAsync() {
     let { Paths, ArtifactConfig } = configConstantsFactory();
     let resolve = utils.createResolverForPath(Paths.dir);
     let artifactResolve = utils.createResolverForPath(resolve(Paths.artifactDirRelativeName));
@@ -12,14 +14,14 @@ function run() {
     let routes = utils.getFromJsonFile(routesPath);
 
     let ext = ".xml";
-    let SitemapPaths = {
+    let sitemapPaths = {
         dir: resolve("./static/sitemaps"),
         index: resolve("./static/sitemap" + ext),
     }
     
     let mainOpts = {
         hostname: 'https://www.prasannavl.com',
-        filename: "core",
+        name: "core",
         urls: routes
             .filter(x => !x.routeTypeCode)
             .map(routeToSitemapFilter)
@@ -33,12 +35,24 @@ function run() {
             .map(routeToSitemapFilter)
     };
 
-    [mainOpts, contentOpts].forEach(x => {
-        let sm = sitemap.createSitemap(x);
-        sm.toXML(function (err, xml) {
-            if (err) throw err;
+    const opts = [mainOpts, contentOpts];
+
+    return fs.ensureDirAsync(sitemapPaths.dir)
+        .then(() => {
+            let tasks = opts.map(x => {
+                console.log(chalk.cyan("generating sitemaps.."));
+                let sm = sitemap.createSitemap(x);
+                sm.toXML(function (err, xml) {
+                    if (err) throw err;
+                    return fs.writeFileAsync(path.join(sitemapPaths.dir, x.name + ext), xml,
+                        { encoding: "utf-8", flag: "w+" });
+                });
+            });
+            return Promise.all(tasks);
+        }).then(() => {
+            console.log(chalk.cyan("generating sitemap index.."));
+            createSitemapIndex(sitemapPaths.index, opts);
         });
-    });
 }
 
 function routeToSitemapFilter(routeObject) {
@@ -49,4 +63,8 @@ function routeToSitemapFilter(routeObject) {
     }
 }
 
-run();
+function createSitemapIndex(indexFilePath, opts) {
+    
+}
+
+runAsync();
