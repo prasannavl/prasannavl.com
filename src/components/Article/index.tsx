7bfb53384c9e3e21822634acbea0e56eb65e68f4
Prasanna V. Loganathar <pvl@prasannavl.com>
2016-06-19 01:33:02 +0530

    Remove Google ads

--- src/components/Article/index.tsx
+++ src/components/Article/index.tsx
@@ -4,7 +4,6 @@ import marked from "marked";
 import createStyled from "../../modules/core/createStyled";
 import Footer from "../fragments/Footer";
 import { loadComments } from "../../modules/ext/disqus";
-import { show as showGoogleAds } from "../../modules/ext/googleAdSense";
 import Rx from "rxjs";
 import ReactDOM from "react-dom";
 import Link from "../fragments/Link";
@@ -51,8 +50,7 @@ export class Article extends StatelessBase<ArticleProps> {
 
     onUpdate() {
         this._articleDomElements.forEach(x => ViewUtils.captureRouteLinks(this, x));
-        setTimeout(() => showGoogleAds(), 20);
-        setTimeout(() => loadComments(this.getCurrentHistoryContext().pathname, this.props.data.name), 40);   
+        setTimeout(() => loadComments(this.getCurrentHistoryContext().pathname, this.props.data.name), 20);   
     }
 
     componentWillUnmount() {
@@ -74,32 +72,6 @@ export class Article extends StatelessBase<ArticleProps> {
         </header>);
     }
 
-    // renderMediaAdFromGoogle() {
-    //     return (
-    //         <ins className="adsbygoogle ads-text"
-    //             style={{ display: "block"  }}
-    //             data-ad-client="ca-pub-1693387204520380"
-    //             data-ad-slot="8634084491"
-    //             data-ad-format="auto"></ins>
-    //     );
-    // }
-
-    // renderMediaAd2FromGoogle() {
-    //     return (
-    //         <ins className="adsbygoogle"
-    //             style={{ display: "inline-block", width: "728px", height: "90px" }}
-    //             data-ad-client="ca-pub-1693387204520380"
-    //             data-ad-slot="1110817699"></ins>
-    //     );
-    // }
-
-    renderTextAdFromGoogle() {
-        return (<ins className="adsbygoogle ads-text"
-            style={{ display: "block" }}
-            data-ad-client="ca-pub-1693387204520380" data-ad-slot="5530147693" data-ad-format="link">
-        </ins>);
-    }
-
     render() {
         this._articleDomElements = new Array<HTMLElement>();
         let item = this.props.data;
@@ -111,7 +83,6 @@ export class Article extends StatelessBase<ArticleProps> {
                         <article dangerouslySetInnerHTML={{ __html: marked(item.content) }} ref={(r) => r && this._articleDomElements.push(r) }></article>
                     </main>
                     <div ref="extContainer" className="ext-container">
-                        { this.renderTextAdFromGoogle() }
                         <div id="disqus_thread"></div>
                     </div>
                     <Footer/>
