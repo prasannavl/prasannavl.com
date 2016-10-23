55bdfbd030e6301d181935f00a88a8b9dc428685
Prasanna V. Loganathar <pvl@prasannavl.com>
2016-10-24 00:13:48 +0530

    content: use triplet tagline

--- src/components/fragments/Tagline.tsx
+++ src/components/fragments/Tagline.tsx
@@ -4,7 +4,7 @@ import { StatelessBase } from "../Base";
 export default class Header extends StatelessBase<any> {
     render() {
         const c = (
-            <h2 {...this.props}><span>A 20-something </span><a href="https://xkcd.com/242/" target="_blank">mad-man</a>, and a <a href="https://xkcd.com/387/" target="_blank">technology architect</a>.<span></span></h2>
+            <h2 {...this.props}><span>A 20-something </span><a href="https://xkcd.com/242/" target="_blank">mad-man</a>, <a href="https://xkcd.com/387/" target="_blank">technology architect</a>, and <a href="https://xkcd.com/85/" target="_blank">obsessionist</a>.<span></span></h2>
         );
         return c;
     }
