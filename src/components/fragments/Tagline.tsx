d9d9ef9da50678f84be58a7d343487b99030e618
Prasanna V. Loganathar <pvl@prasannavl.com>
2016-10-23 19:44:22 +0530

    content: change ambiguous tagline

--- src/components/fragments/Tagline.tsx
+++ src/components/fragments/Tagline.tsx
@@ -4,7 +4,7 @@ import { StatelessBase } from "../Base";
 export default class Header extends StatelessBase<any> {
     render() {
         const c = (
-            <h2 {...this.props}><span>A </span><a href="https://xkcd.com/242/" target="_blank">mad-man</a> <span>with a computer, walking the grove between </span><a href="https://what-if.xkcd.com/" target="_blank">Science</a><span> and </span><a href="https://xkcd.com/387/" target="_blank">Technology</a><span>.</span></h2>
+            <h2 {...this.props}><span>A 20-something </span><a href="https://xkcd.com/242/" target="_blank">mad-man</a>, <a href="https://xkcd.com/387/" target="_blank">technology architect</a> and <a href="https://www.forbes.com/sites/kateharrison/2013/03/05/cartoon-truths-for-entrepreneurs/#286e22733af4">entrepreneur</a>, in that order.<span></span></h2>
         );
         return c;
     }
