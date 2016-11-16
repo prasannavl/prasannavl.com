ce83a1e40ac08f5bcef17066d8f9629504c3874b
Prasanna V. Loganathar <pvl@prasannavl.com>
2016-11-16 22:20:33 +0530

    content: tone down the tagline

--- src/components/fragments/Tagline.tsx
+++ src/components/fragments/Tagline.tsx
@@ -4,7 +4,7 @@ import { StatelessBase } from "../Base";
 export default class Header extends StatelessBase<any> {
     render() {
         const c = (
-            <h2 {...this.props}><span>A 20-something </span><a href="https://xkcd.com/242/" target="_blank">mad-man</a>, <a href="https://xkcd.com/387/" target="_blank">technology architect</a>, and <a href="https://www.forbes.com/sites/kateharrison/2013/03/05/cartoon-truths-for-entrepreneurs/#286e22733af4" target="_blank">entrepreneur</a>.<span></span></h2>
+            <h2 {...this.props}><span>A 20-something </span><a href="https://xkcd.com/242/" target="_blank">science-geek</a>, <a href="https://xkcd.com/387/" target="_blank">technology architect</a> and <a href="https://www.forbes.com/sites/kateharrison/2013/03/05/cartoon-truths-for-entrepreneurs/#286e22733af4" target="_blank">entrepreneur</a>.<span></span></h2>
         );
         return c;
     }
