7bc96ac7d2a0e839e7638878715fde5f417eb36e
Prasanna V. Loganathar <pvl@prasannavl.com>
2016-12-04 14:59:13 +0530

    change: encoded mail address component

--- src/components/fragments/Address.tsx
+++ src/components/fragments/Address.tsx
@@ -3,19 +3,33 @@ import ReactDOM from "react-dom";
 
 class Address extends React.Component<any, any> {
     render() {
-        //const emailAddress = "&#112;&#118;&#108;&#064;&#112;&#114;&#097;&#115;&#097;&#110;&#110;&#097;&#118;&#108;&#046;&#099;&#111;&#109;";
-        const emailAddress = "pvl@prasannavl.com";
-        const mailToAddress = "mailto:Prasanna V. Loganathar <" + emailAddress + ">";
+        const encodedEmailAddress = "&#x70;&#x76;&#x6c;&#x40;&#x70;&#x72;&#x61;&#x73;&#x61;&#x6e;&#x6e;&#x61;&#x76;&#x6c;&#x2e;&#x63;&#x6f;&#x6d;";
+        const mailToHref = `mailto:${encodedEmailAddress}`;
+        const str = `
+                <a href="https://www.twitter.com/prasannavl" class="icon-twitter" target="_blank"></a>
+                <a href="https://www.github.com/prasannavl" class="icon-mark-github" target="_blank"></a>
+                <a href="https://www.facebook.com/prasannavl" class="icon-facebook-square" target="_blank"></a>
+                <a class="icon-envelope" href="${mailToHref}"></a>
+                `;
+        return (<address className="icons" dangerouslySetInnerHTML={{ __html: str}}></address>);
+    }
+
+/**
+    // If React can someday handle raw attribute value, use this below:
+    render() {
+        const encodedEmailAddress = "&#x70;&#x76;&#x6c;&#x40;&#x70;&#x72;&#x61;&#x73;&#x61;&#x6e;&#x6e;&#x61;&#x76;&#x6c;&#x2e;&#x63;&#x6f;&#x6d;";
+        const mailToHref = `mailto:${encodedEmailAddress}`;
         const c = (
             <address className="icons">
                 <a href="https://www.twitter.com/prasannavl" className="icon-twitter" target="_blank"></a>
                 <a href="https://www.github.com/prasannavl" className="icon-mark-github" target="_blank"></a>
                 <a href="https://www.facebook.com/prasannavl" className="icon-facebook-square" target="_blank"></a>
-                <a href={mailToAddress} className="icon-envelope"></a>
+                <a href={mailToHref} className="icon-envelope"></a>
             </address>
         );
         return c;
     }
+**/
 }
 
 export default Address;
