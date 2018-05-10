06c93b024e3bcdcad8d5e53d2c54e3f98e18afbd
Prasanna Loganathar <pvl@prasannavl.com>
2018-05-10 12:58:34 +0530

    content: fix code block alignments

--- src/posts/2016/10/introducing-winapi-basics.jsx
+++ src/posts/2016/10/introducing-winapi-basics.jsx
@@ -15,14 +15,14 @@ export default () => {
         <p>In the previous article <Link to="/2016/10/introducing-winapi-the-evolution/">here</Link>, I discussed the evolution of programs that use the Windows API with C/C++ and C# snippets, and it ultimately ended out with this C# snippet:</p>
 
         <CodeBlock children={`
-    static int Main(string[] args)
+static int Main(string[] args)
+{
+    using (var win = Window.Create(text: "Hello"))
     {
-        using (var win = Window.Create(text: "Hello"))
-        {
-            win.Show();
-            return new EventLoop().Run(win);
-        }
+        win.Show();
+        return new EventLoop().Run(win);
     }
+}
         `} />
 
 
@@ -31,92 +31,92 @@ export default () => {
         <p>A very raw program that uses the Windows API would look like this:</p>
 
         <CodeBlock children={`
-    using System;
-    using System.Runtime.InteropServices;
-    using WinApi.Gdi32;
-    using WinApi.Kernel32;
-    using WinApi.User32;
-    
-    namespace Sample.Win32
+using System;
+using System.Runtime.InteropServices;
+using WinApi.Gdi32;
+using WinApi.Kernel32;
+using WinApi.User32;
+
+namespace Sample.Win32
+{
+    internal class Program
     {
-        internal class Program
+        static int Main(string[] args)
         {
-            static int Main(string[] args)
+            var instanceHandle = Kernel32Methods.GetModuleHandle(IntPtr.Zero);
+
+            var wc = new WindowClassEx
             {
-                var instanceHandle = Kernel32Methods.GetModuleHandle(IntPtr.Zero);
-    
-                var wc = new WindowClassEx
-                {
-                    Size = (uint) Marshal.SizeOf<WindowClassEx>(),
-                    ClassName = "MainWindow",
-                    CursorHandle = User32Helpers.LoadCursor(IntPtr.Zero, SystemCursor.IDC_ARROW),
-                    IconHandle = User32Helpers.LoadIcon(IntPtr.Zero, SystemIcon.IDI_APPLICATION),
-                    Styles = WindowClassStyles.CS_HREDRAW | WindowClassStyles.CS_VREDRAW,
-                    BackgroundBrushHandle = new IntPtr((int) StockObject.WHITE_BRUSH),
-                    WindowProc = WindowProc,
-                    InstanceHandle = instanceHandle
-                };
-    
-                var resReg = User32Methods.RegisterClassEx(ref wc);
-                if (resReg == 0)
-                {
-                    Console.Error.WriteLine("registration failed");
-                    return -1;
-                }
-    
-                var hwnd = User32Methods.CreateWindowEx(WindowExStyles.WS_EX_APPWINDOW,
-                    wc.ClassName, "Hello", WindowStyles.WS_OVERLAPPEDWINDOW,
-                    (int) CreateWindowFlags.CW_USEDEFAULT, (int) CreateWindowFlags.CW_USEDEFAULT,
-                    (int) CreateWindowFlags.CW_USEDEFAULT, (int) CreateWindowFlags.CW_USEDEFAULT,
-                    IntPtr.Zero, IntPtr.Zero, instanceHandle, IntPtr.Zero);
-    
-                if (hwnd == IntPtr.Zero)
-                {
-                    Console.Error.WriteLine("window creation failed");
-                    return -1;
-                }
-    
-                User32Methods.ShowWindow(hwnd, ShowWindowCommands.SW_SHOWNORMAL);
-                User32Methods.UpdateWindow(hwnd);
-    
-                Message msg;
-                int res;
-                while ((res = User32Methods.GetMessage(out msg, IntPtr.Zero, 0, 0)) != 0)
-                {
-                    User32Methods.TranslateMessage(ref msg);
-                    User32Methods.DispatchMessage(ref msg);
-                }
-    
-                return res;
-            }
-    
-            private static IntPtr WindowProc(IntPtr hwnd, uint umsg,
-                IntPtr wParam, IntPtr lParam)
+                Size = (uint) Marshal.SizeOf<WindowClassEx>(),
+                ClassName = "MainWindow",
+                CursorHandle = User32Helpers.LoadCursor(IntPtr.Zero, SystemCursor.IDC_ARROW),
+                IconHandle = User32Helpers.LoadIcon(IntPtr.Zero, SystemIcon.IDI_APPLICATION),
+                Styles = WindowClassStyles.CS_HREDRAW | WindowClassStyles.CS_VREDRAW,
+                BackgroundBrushHandle = new IntPtr((int) StockObject.WHITE_BRUSH),
+                WindowProc = WindowProc,
+                InstanceHandle = instanceHandle
+            };
+
+            var resReg = User32Methods.RegisterClassEx(ref wc);
+            if (resReg == 0)
             {
-                var msg = (WM) umsg;
-                switch (msg)
-                {
-                    case WM.ERASEBKGND:
-                        return new IntPtr(1);
-                    case WM.CLOSE:
-                    {
-                        User32Methods.PostQuitMessage(0);
-                        break;
-                    }
-                    case WM.PAINT:
-                    {
-                        PaintStruct ps;
-                        var hdc = User32Methods.BeginPaint(hwnd, out ps);
-                        User32Methods.FillRect(hdc, ref ps.PaintRect,
-                            Gdi32Helpers.GetStockObject(StockObject.WHITE_BRUSH));
-                        User32Methods.EndPaint(hwnd, ref ps);
-                        break;
-                    }
-                }
-                return User32Methods.DefWindowProc(hwnd, umsg, wParam, lParam);
+                Console.Error.WriteLine("registration failed");
+                return -1;
             }
+
+            var hwnd = User32Methods.CreateWindowEx(WindowExStyles.WS_EX_APPWINDOW,
+                wc.ClassName, "Hello", WindowStyles.WS_OVERLAPPEDWINDOW,
+                (int) CreateWindowFlags.CW_USEDEFAULT, (int) CreateWindowFlags.CW_USEDEFAULT,
+                (int) CreateWindowFlags.CW_USEDEFAULT, (int) CreateWindowFlags.CW_USEDEFAULT,
+                IntPtr.Zero, IntPtr.Zero, instanceHandle, IntPtr.Zero);
+
+            if (hwnd == IntPtr.Zero)
+            {
+                Console.Error.WriteLine("window creation failed");
+                return -1;
+            }
+
+            User32Methods.ShowWindow(hwnd, ShowWindowCommands.SW_SHOWNORMAL);
+            User32Methods.UpdateWindow(hwnd);
+
+            Message msg;
+            int res;
+            while ((res = User32Methods.GetMessage(out msg, IntPtr.Zero, 0, 0)) != 0)
+            {
+                User32Methods.TranslateMessage(ref msg);
+                User32Methods.DispatchMessage(ref msg);
+            }
+
+            return res;
+        }
+
+        private static IntPtr WindowProc(IntPtr hwnd, uint umsg,
+            IntPtr wParam, IntPtr lParam)
+        {
+            var msg = (WM) umsg;
+            switch (msg)
+            {
+                case WM.ERASEBKGND:
+                    return new IntPtr(1);
+                case WM.CLOSE:
+                {
+                    User32Methods.PostQuitMessage(0);
+                    break;
+                }
+                case WM.PAINT:
+                {
+                    PaintStruct ps;
+                    var hdc = User32Methods.BeginPaint(hwnd, out ps);
+                    User32Methods.FillRect(hdc, ref ps.PaintRect,
+                        Gdi32Helpers.GetStockObject(StockObject.WHITE_BRUSH));
+                    User32Methods.EndPaint(hwnd, ref ps);
+                    break;
+                }
+            }
+            return User32Methods.DefWindowProc(hwnd, umsg, wParam, lParam);
         }
     }
+}
         `} />
 
         <p>Why would one do this? Because you can. You have no reason to go this really tedious way, however - if you want you always can. The primary function of <code>WinApi</code> is after-all to provide a clean high-performance interop story.</p>
@@ -138,28 +138,28 @@ export default () => {
         <p>As an example <code>User32Methods</code> has the following method defined:</p>
 
         <CodeBlock children={`
-    [DllImport(LibraryName, ExactSpelling = true)]
-    public static extern int MapWindowPoints(IntPtr hWndFrom, IntPtr hWndTo, IntPtr lpPoints, int cPoints);
+[DllImport(LibraryName, ExactSpelling = true)]
+public static extern int MapWindowPoints(IntPtr hWndFrom, IntPtr hWndTo, IntPtr lpPoints, int cPoints);
         `} />
 
         <p>It takes all inputs in the form of <code>IntPtr</code>, which allows any kind of marshalling.</p>
         <p>At the same time, <code>User32Helpers</code> has the following implementations:</p>
 
         <CodeBlock children={`
-    public static unsafe int MapWindowPoints(IntPtr hWndFrom, IntPtr hWndTo, ref Point point)
-    {
-        fixed (Point* ptr = &point)
-            return User32Methods.MapWindowPoints(hWndFrom, hWndTo, new IntPtr(ptr), 1);
-    }
+public static unsafe int MapWindowPoints(IntPtr hWndFrom, IntPtr hWndTo, ref Point point)
+{
+    fixed (Point* ptr = &point)
+        return User32Methods.MapWindowPoints(hWndFrom, hWndTo, new IntPtr(ptr), 1);
+}
     
-    public static unsafe int MapWindowPoints(IntPtr hWndFrom, IntPtr hWndTo, ref Rectangle rect)
+public static unsafe int MapWindowPoints(IntPtr hWndFrom, IntPtr hWndTo, ref Rectangle rect)
+{
+    fixed (Rectangle* ptr = &rect)
     {
-        fixed (Rectangle* ptr = &rect)
-        {
-            var ptPtr = (Point*) ptr;
-            return User32Methods.MapWindowPoints(hWndFrom, hWndTo, new IntPtr(ptPtr), 2);
-        }
+        var ptPtr = (Point*) ptr;
+        return User32Methods.MapWindowPoints(hWndFrom, hWndTo, new IntPtr(ptPtr), 2);
     }
+}
         `} />
 
         <p>Again, this provides straight forward Marshalling without any performance impact, since it simply pins the struct which already have C-Layouts, and passes in the pointers.</p>
@@ -176,11 +176,11 @@ export default () => {
         <p>This is the class that is the thinnest layer of Window. It simply wraps the original <code>Win32</code> handle. And it only has a single member - an IntPtr of <code>Handle</code>, and provides a way to attach itself to any Handle, and allows nice wrappers to be used through out.</p>
 
         <CodeBlock children={`
-    var win = WindowFactory.CreateWindowFromHandle(someHwnd);
-    win.SetText("Hello");
-    win.SetPosition(100, 200);
-    win.Show();
-    win.Close();
+var win = WindowFactory.CreateWindowFromHandle(someHwnd);
+win.SetText("Hello");
+win.SetPosition(100, 200);
+win.Show();
+win.Close();
         `} />
 
         <h3>Class: <code>WindowFactory</code></h3>
@@ -206,20 +206,20 @@ export default () => {
         <p>For example</p>
 
         <CodeBlock children={`
-    public sealed class MainWindow : EventedWindowCore
+public sealed class MainWindow : EventedWindowCore
+{
+    protected override void OnCreate(ref CreateWindowPacket packet)
     {
-        protected override void OnCreate(ref CreateWindowPacket packet)
-        {
-            base.OnCreate(ref packet);
-        }
-    
-        protected override void OnSize(ref SizePacket packet)
-        {
-            var size = packet.Size;
-            var flags = packet.Flag;
-            base.OnSize(ref packet);
-        }
+        base.OnCreate(ref packet);
     }
+
+    protected override void OnSize(ref SizePacket packet)
+    {
+        var size = packet.Size;
+        var flags = packet.Flag;
+        base.OnSize(ref packet);
+    }
+}
         `} />
 
         <p>Internally it uses the <code>Packetizer</code> class that simples create a corresponding <code>Packet</code> struct, and transparently decodes every message into its parameters. The <code>EventedWindowCore</code> handles most of the common window messages. That's actually all it does. The implementation can be thought of as nothing but one giant switch case that does simply creates a packet and propagates the packet to its appropriate handler method.</p>
@@ -227,14 +227,14 @@ export default () => {
         <p>For example,</p>
 
         <CodeBlock children={`
-    public static unsafe void ProcessMove(ref WindowMessage msg, EventedWindowCore window)
+public static unsafe void ProcessMove(ref WindowMessage msg, EventedWindowCore window)
+{
+    fixed (WindowMessage* ptr = &msg)
     {
-        fixed (WindowMessage* ptr = &msg)
-        {
-            var packet = new MovePacket(ptr);
-            window.OnMove(ref packet);
-        }
+        var packet = new MovePacket(ptr);
+        window.OnMove(ref packet);
     }
+}
         `} />
 
         <p>This is the implementation for <code>WM_MOVE</code> handler. The <code>MovePacket</code> provides a very nice way to handle <code>WM_MOVE</code>, since it can both decode or encode the WM_MOVE messages into its parameter. It has the property <code>Point</code> as one of its properties.</p>
@@ -246,29 +246,29 @@ export default () => {
         <p>Infact, the way <code>EventedWindowCore</code> is implemented very similar to this:</p>
 
         <CodeBlock children={`
-    public class EventedWindowCore : WindowCore {
-        protected override OnMessage(ref WindowMessage msg) {
-            switch (msg.Id) {
-                ...
-                case WM.MOVE {
-                    Packetizer.ProcessMove(ref msg, this);
-                    break;
-                }
-                ...
-                default:
-                {
-                    this.OnMessageDefault(ref msg);
-                }
+public class EventedWindowCore : WindowCore {
+    protected override OnMessage(ref WindowMessage msg) {
+        switch (msg.Id) {
+            ...
+            case WM.MOVE {
+                Packetizer.ProcessMove(ref msg, this);
+                break;
+            }
+            ...
+            default:
+            {
+                this.OnMessageDefault(ref msg);
             }
         }
-    
-        ...
-        protected internal virtual void OnMove(ref MovePacket packet)
-        {
-            // Call the OnMessageDefault here.
-        }
-        ...
     }
+
+    ...
+    protected internal virtual void OnMove(ref MovePacket packet)
+    {
+        // Call the OnMessageDefault here.
+    }
+    ...
+}
         `} />
 
         <p>And in the above case <code>OnMessageDefault</code> translates into calling the base window procedure, which would be the <code>DefWindowProc</code> method in <code>user32.dll</code> if its an plain window, or the window's default procedure if its an in-built class like <code>STATIC</code>, <code>EDIT</code>, etc.</p>
@@ -278,67 +278,67 @@ export default () => {
         <h2>Putting it all together</h2>
 
         <CodeBlock children={`
-    internal class Program
+internal class Program
+{
+    static int Main(string[] args)
     {
-        static int Main(string[] args)
+        // Window is just a wrapper over EventedWindowCore,
+        // that provides more convinience methods, which has
+        // its own self registered factory.
+        //
+        // Window is a part of WinApi.Windows.Controls.
+        //
+        // Samples contain code that also directly initiates
+        // EventedWindowCore without depending on 
+        // WinApi.Windows.Controls simply by creating 
+        // WindowFactory
+        using (var win = Window.Create<AppWindow>("Hello"))
         {
-            // Window is just a wrapper over EventedWindowCore,
-            // that provides more convinience methods, which has
-            // its own self registered factory.
-            //
-            // Window is a part of WinApi.Windows.Controls.
-            //
-            // Samples contain code that also directly initiates
-            // EventedWindowCore without depending on 
-            // WinApi.Windows.Controls simply by creating 
-            // WindowFactory
-            using (var win = Window.Create<AppWindow>("Hello"))
-            {
-                win.Show();
-                return new EventLoop().Run(win);
-            }
+            win.Show();
+            return new EventLoop().Run(win);
         }
     }
-    
-    public class AppWindow : Window
+}
+
+public class AppWindow : Window
+{
+    protected override void OnMove(ref MovePacket packet)
     {
-        protected override void OnMove(ref MovePacket packet)
-        {
-            base.OnMove(ref packet);
-        }
-    
-        protected override void OnMessage(ref WindowMessage msg)
-        {
-            switch (msg.Id)
-            {
-                // Note: OnEraseBkgnd method is already available in 
-                // EventedWindowCore, but directly intercepted here
-                // just for the sake of overriding the
-                // message loop.
-                // Also, note that the message loop is 
-                // short-cicuited here.
-    
-                case WM.ERASEBKGND:
-                {
-                    // I can even build the loop only on pay-per-use
-                    // basis, when I need it since all the Packets decoding,
-                    // and encoding are cleanly abstracted away into the Packet
-                    // structs itself.
-                    //
-                    // fixed (var msgPtr = &msg)
-                    // {
-                    //    var packet = new EraseBkgndPacket(msg);
-                    //    // Do anything you want with the packet.
-                    // }
-                    // return;
-    
-                    msg.Result = new IntPtr(1);
-                    return;
-                }
-            }
-            base.OnMessage(ref msg);
-        }
+        base.OnMove(ref packet);
     }
+
+    protected override void OnMessage(ref WindowMessage msg)
+    {
+        switch (msg.Id)
+        {
+            // Note: OnEraseBkgnd method is already available in 
+            // EventedWindowCore, but directly intercepted here
+            // just for the sake of overriding the
+            // message loop.
+            // Also, note that the message loop is 
+            // short-cicuited here.
+
+            case WM.ERASEBKGND:
+            {
+                // I can even build the loop only on pay-per-use
+                // basis, when I need it since all the Packets decoding,
+                // and encoding are cleanly abstracted away into the Packet
+                // structs itself.
+                //
+                // fixed (var msgPtr = &msg)
+                // {
+                //    var packet = new EraseBkgndPacket(msg);
+                //    // Do anything you want with the packet.
+                // }
+                // return;
+
+                msg.Result = new IntPtr(1);
+                return;
+            }
+        }
+        base.OnMessage(ref msg);
+    }
+}
     `} />
 
         <p>The Samples included in the <code>WinApi</code> repository also has several programs that use DirectX, OpenGL, Skia, to paint windows. There are tons of other things that <code>WinApi</code> does, including one of the simplest ways of using <code>SendInput</code> with the helpers built right in, <code>DxUtils</code> that relives all the pain of managing the numerous Direct3D, Direct2D and also manages its versioning with ease, all while maintaining high-performance without any pressure on the GC.</p>
