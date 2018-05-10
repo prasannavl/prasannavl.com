e6746b19901f1cd2ad7834f40a3423894f72b077
Prasanna Loganathar <pvl@prasannavl.com>
2018-05-10 06:27:02 +0530

    content: add drafts

---
+++ src/posts/drafts/git-common-tasks.jsx
@@ -0,0 +1,40 @@
+import { Article, CodeBlock } from "../../components/Article"; 
+
+export const meta = {
+    title: "Git - Common tasks",
+    tags: ["git"],
+    note: true,
+    date: "2018-05-06T12:48:32.891Z",
+}
+
+export default () => {
+    return <Article {...meta}>
+        <h2>Reset master to a better branch</h2>
+
+        <CodeBlock children={`
+git checkout master
+git reset --hard better_branch
+git push -f origin master       
+        `}/>
+
+        <h2>Merge better branch into master</h2>
+        
+        <CodeBlock children={`
+git checkout better_branch
+git merge --strategy=ours master    # keep the content of this branch, but record a merge
+git checkout master
+git merge better_branch             # fast-forward master up to the merge      
+        `}/>
+
+        <h3>With explicit history</h3>
+
+        <CodeBlock children={`
+git checkout better_branch
+git merge --strategy=ours --no-commit master
+git commit          # add better history info
+git checkout master
+git merge better_branch
+        `} />
+
+    </Article>
+}
---
+++ src/posts/drafts/git-credential.jsx
@@ -0,0 +1,26 @@
+import { Article, CodeBlock, Link } from "../../components/Article";
+
+export const meta = {
+    title: "Git - Credential helper",
+    tags: ["git", "ubuntu"],
+    note: true,
+    date: "2018-05-06T11:20:53.320Z",
+}
+
+export default () => {
+    return <Article {...meta}>
+        <h2>Setup on Ubuntu</h2>
+
+        <h3>Compile the keyring</h3>
+        <CodeBlock children={`
+sudo apt-get install libgnome-keyring-dev
+sudo make --directory=/usr/share/doc/git/contrib/credential/gnome-keyring
+        `} />
+
+        <h3>Setup config</h3> 
+        <CodeBlock children={`
+git config --global credential.helper /usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring
+        `} />
+
+    </Article>
+}
