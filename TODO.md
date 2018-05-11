5f495236452743f4a59b103789655dc9dd01deb3
Prasanna Loganathar <pvl@prasannavl.com>
2018-05-11 21:37:40 +0530

    fix: link default events

--- TODO.md
+++ TODO.md
@@ -1,7 +1,5 @@
 # TODO
 
-- Fix <Link> for external invocation
-- Fix <Link> for Ctrl+Click
 - Add website source link (in Projects?)
 - Drafts view
 - content-manager: publish
--- src/modules/router-utils.js
+++ src/modules/router-utils.js
@@ -1,38 +1,54 @@
 import React from "react";
 import { trimRightSlashes } from "./path-utils";
 
-export const Link = ({ href, to, passHref, scroll, tag, replace, children, ...rest }) => {
+export const Link = ({ href, to, passHref, scroll, tag, replace, onClick, children, ...rest }) => {
     let itemTag = tag || "a";
     if (scroll !== false) {
         scroll = true;
     }
-    let props = { ...rest, onClick: linkClickHandler, "data-scroll": scroll, "data-replace": replace };
+    const finalHref = href || to;
+
+    let props = {
+        ...rest,
+        onClick: getDefaultLinkHandler({ 
+            scroll,
+            replace,
+            onClick,
+            href: finalHref,
+        })
+    };
     // If `a` tag, or if passHref is true, set the href prop, 
     // or else, set it data-href attribute.
     if (itemTag === "a" || passHref) {
-        Object.assign(props, { href: href || to });
+        Object.assign(props, { href: finalHref });
     } else {
-        Object.assign(props, { "data-href": href || to });
+        Object.assign(props, { "data-href": finalHref });
     }
     return React.createElement(itemTag, props, children);
 }
 
-const linkClickHandler = (event) => {
-    event.preventDefault();
-    let target = event.currentTarget;
-    const path = target.href || target.getAttribute("data-href");
-    const scroll = target.getAttribute("data-scroll");
-    const replace = target.getAttribute("data-replace");
-    // use timeout to keep screen responsive on mobile/low-end devices.
-    setTimeout(() => {
-        if (replace ||
-            (trimRightSlashes(path) === trimRightSlashes(window.location.href)
-                && !window.location.search
-                && !window.location.hash)) {
-            window.history.replaceState(null, undefined, path);
-        } else {
-            window.history.pushState(null, undefined, path);
-        }
-        window.dispatchEvent(new PopStateEvent('popstate', { state: { scroll } }));
-    }, 0);
+export function shouldDispatchDefaultClickEvent(e) {
+    return (e.button !== 0 || e.altKey || e.ctrlKey || e.metaKey || e.shiftKey);
+}
+
+export function getDefaultLinkHandler(props) {
+    return function defaultLinkHandler(event) {
+        if (shouldDispatchDefaultClickEvent(event)) return;
+        const { scroll, replace, onClick, href: path } = props;
+        if (onClick) onClick(event);
+        if (event.defaultPrevented) return;
+        event.preventDefault();
+        // use timeout to keep screen responsive on mobile/low-end devices.
+        setTimeout(() => {
+            if (replace ||
+                (trimRightSlashes(path) === trimRightSlashes(window.location.href)
+                    && !window.location.search
+                    && !window.location.hash)) {
+                window.history.replaceState(null, undefined, path);
+            } else {
+                window.history.pushState(null, undefined, path);
+            }
+            window.dispatchEvent(new PopStateEvent('popstate', { state: { scroll } }));
+        }, 0);
+    }
 }
