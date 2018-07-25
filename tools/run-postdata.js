828615727563e0b9e395cfa8c2e0ae0e4c2fcd31
Prasanna Loganathar <pvl@prasannavl.com>
2018-07-25 22:03:36 +0530

    fix: data generation gap

--- tools/run-postdata.js
+++ tools/run-postdata.js
@@ -1,7 +1,9 @@
+const routesGen = require("./generate-routes");
 const sitemapGen = require("./generate-sitemap");
 const rssGen = require("./generate-rss");
 
 async function runAsync() {
+    await routesGen.runAsync();
     await Promise.all([sitemapGen.runAsync(), rssGen.runAsync()]);
 }
 
--- tools/run-predata.js
+++ tools/run-predata.js
@@ -1,9 +1,7 @@
 const dataGen = require("./generate-data");
-const routesGen = require("./generate-routes");
 
 async function runAsync() {
     await dataGen.runAsync();
-    await routesGen.runAsync();
 }
 
 runAsync();
